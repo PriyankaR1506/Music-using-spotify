fe8ebe85f8846a6fc52fd5e3266e0f967ab688ac
Aaron Wang <desmond.w9481@gmail.com>
2016-07-28 15:32:22 -0400

    Initial Commit

---
+++ app/src/main/java/yezhenwang/newgenerationspotify/SignInPage.java
@@ -0,0 +1,14 @@
+package yezhenwang.newgenerationspotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page);
+    }
+}
+
